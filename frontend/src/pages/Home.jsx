9c3545de8929131aa76bef3c2ad932305b7014e0
peter-mwau <kyalepeter2000@gmail.com>
2026-01-18 19:51:31 +0300

    initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="bg-gray-300 h-screen pt-[100px] text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to the Home Page</h1>
+      <p className="text-lg">This is a simple home page component.</p>
+    </div>
+  );
+}
+
+export default Home;
